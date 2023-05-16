93a7a201a451fdcdea216bc6122f879db8b3ca55
Priyam Das <domidas01@gmail.com>
2023-05-16 06:28:33 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(arr, e){
+    const newArr = [];
+    for(let i = 0; i< arr.length; i++){
+        newArr[i] = `Thank you, ${arr[i]}, for the wonderful surprise gift!`
+    }
+    return newArr;
+}
+function countDown(num){
+    while(num>=0){
+        console.log(num);
+        num--;
+    }
+}
